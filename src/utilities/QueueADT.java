f201b405a459b4bb9af984c8d3d8eb1f486640c4
Hoa Le <Hoa.le@edu.sait.ca>
2022-10-12 14:41:06 -0600

    Some documents for StackADT

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,10 +1,14 @@
 package utilities;
 
 /**
- * Class description: 
+ * <p>
+ * The <code>QueueADT</code> interface is designed to be used as a basis for all
+ * the Linear data structures that will be developed in the CPRG 311 class at
+ * SAIT. The implementors of this interface will be required to add all the
+ * functionality.
+ * </p>
  *
- * @author Weilong Mao
- *
+ * @param <E> The type of elements this list holds.
  */
 public interface QueueADT<E> {
 
--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -1,32 +1,102 @@
 package utilities;
 
+import java.io.Serializable;
+import java.util.EmptyStackException;
 
 /**
- * 
- * Class description: 
+ * <p>
+ * The <code>StackADT</code> interface is designed to be used as a basis for all
+ * the Stack Data Structure that will be developed in assignment 2 in the CPRG 311 at
+ * SAIT. The implementors of this interface will be required to add all the
+ * functionality.
+ * </p>
  *
- * @author Weilong Mao
- *
+ * @param <E> The type of elements this list holds.
  */
-public interface StackADT<E> {
+public interface StackADT <E> extends Serializable {
 
+	/**
+	 * Adds one element to the top of this stack.
+	 * Precondition: The stack is not full.
+	 * Postcondtion: The element has been added to the top of the stack.
+	 *
+	 * @param element the element to be added to the top of this stack
+	 * @throws EmptyStackException if the stack is empty
+	 */
+	public void push (E element) throws EmptyStackException;
+
+	/**
+	 * Removes and returns the top element from this stack.
+	 * precondition:
+	 * postcondition:
+	 * @return the element removed from the top of this stack
+	 * @throws EmptyStackException if this stack is empty
+	 */
+	public E pop() throws EmptyStackException;
+
+	/**
+	 * Returns without removing the top element of this stack.
+	 * @return the first element in this stack
+	 */
 	public E peek();
 
-	public void push(E e);
+	/**
+	 * Returns true if this stack contains no elements.
+	 * @return true if this stack is empty
+	 */
+	public boolean isEmpty();
 
-	public E pop();
-
+	/**
+	 * Returns the number of elements in this stack.
+	 * @return the integer representation of the size of this stack
+	 */
 	public int size();
 
-	public Iterator<E> iterator();
+	/**
+	 *
+	 * @param that
+	 * @return
+	 */
 
-	public boolean isEmpty();
+	public boolean equals(StackADT <E> that);
+
+	/**
+	 * Removes all of the elements from this stack. This stack will be empty after
+	 * this call returns.
+	 */
 
 	public void clear();
 
-	public boolean equals(StackADT<E> that);
+	/**
+	 *
+	 * @return
+	 */
 
-	public E[] toArray();
+	public Object[] toArray();
+
+	/**
+	 *
+	 * @param copy
+	 * @return
+	 */
 
 	public E[] toArray(E[] copy);
+
+	/**
+	 *
+	 * @param element
+	 * @return
+	 */
+	public int search (E element);
+
+	/**
+	 *
+	 * @param element
+	 * @return
+	 */
+
+	public boolean contains (E element);
+
+
+
 }
