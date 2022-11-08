cc2414430ad349b705defa4b2b6e2958cfbf2a19
Hoa Le <Hoa.le@edu.sait.ca>
2022-11-07 21:16:06 -0700

    Updated Driver

--- src/driver/AppDriver.java
+++ src/driver/AppDriver.java
@@ -11,32 +11,41 @@ import xmltools.XMLParser;
  */
 public class AppDriver {
     public static void main(String[] args) {
-        String PATH = args[0];
-        System.out.println("Checking XML file: " + PATH);
-        long startTime = System.currentTimeMillis();
-        XMLParser parser = new XMLParser(PATH);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Finished checking XML file: " + PATH + " with " + (endTime - startTime) + "ms");
-        System.out.println(
-                "        _   _         _  _          _   __ _\n" +
-                        "       | | | |  ___  | || |  ___   | | / /(_)  _     _\n" +
-                        "       | |_| | / _ \\ | || | / _ \\  | |/ /  _ _| |_ _| |_  _  _\n" +
-                        "       |  _  |/ /_\\ \\| || |/ / \\ \\ |   /  | |_   _|_   _|| |/ /\n" +
-                        "       | | | |\\ ,___/| || |\\ \\_/ / | |\\ \\ | | | |_  | |_ | / /\n" +
-                        "       |_| |_| \\___/ |_||_| \\___/  |_| \\_\\|_| \\___| \\___||  /\n" +
-                        "                              _           _              / /\n" +
-                        "                             / \\_______ /|_\\             \\/\n" +
-                        "                            /          /_/ \\__\n" +
-                        "                           /             \\_/ /\n" +
-                        "                         _|_              |/|_\n" +
-                        "                         _|_  O    _    O  _|_\n" +
-                        "                         _|_      (_)      _|_\n" +
-                        "                          \\                 /\n" +
-                        "                           _\\_____________/_\n" +
-                        "                          /  \\/  (___)  \\/  \\\n" +
-                        "                          \\__(  o     o  )__/\n" +
-                        "Team Aerith:\n" +
-                        "            Hoa Le, Weilong Mao, Chris Wang, Hu Peng\n" +
-                        "Ver: 2022-11-06");
+        boolean error = false;
+        String fileName = args[0];
+        if(!fileName.startsWith("-") || fileName.toLowerCase().charAt(1)!= 'f') {
+            System.out.println("Invalid command line argument, the command line argument should be -f<filename>");
+            error = true;
+        }
+        if (!error) {
+            String path = fileName.substring(2).toLowerCase();
+            System.out.println("Checking XML file: " + path);
+            long startTime = System.currentTimeMillis();
+            XMLParser parser = new XMLParser(path);
+            long endTime = System.currentTimeMillis();
+            System.out.println("Finished checking XML file: " + path + " with " + (endTime - startTime) + "ms");
+            System.out.println(
+                    "        _   _         _  _          _   __ _\n" +
+                            "       | | | |  ___  | || |  ___   | | / /(_)  _     _\n" +
+                            "       | |_| | / _ \\ | || | / _ \\  | |/ /  _ _| |_ _| |_  _  _\n" +
+                            "       |  _  |/ /_\\ \\| || |/ / \\ \\ |   /  | |_   _|_   _|| |/ /\n" +
+                            "       | | | |\\ ,___/| || |\\ \\_/ / | |\\ \\ | | | |_  | |_ | / /\n" +
+                            "       |_| |_| \\___/ |_||_| \\___/  |_| \\_\\|_| \\___| \\___||  /\n" +
+                            "                              _           _              / /\n" +
+                            "                             / \\_______ /|_\\             \\/\n" +
+                            "                            /          /_/ \\__\n" +
+                            "                           /             \\_/ /\n" +
+                            "                         _|_              |/|_\n" +
+                            "                         _|_  O    _    O  _|_\n" +
+                            "                         _|_      (_)      _|_\n" +
+                            "                          \\                 /\n" +
+                            "                           _\\_____________/_\n" +
+                            "                          /  \\/  (___)  \\/  \\\n" +
+                            "                          \\__(  o     o  )__/\n" +
+                            "Team Aerith:\n" +
+                            "            Hoa Le, Weilong Mao, Chris Wang, Hu Peng\n" +
+                            "Ver: 2022-11-06");
+        }
+
     }
 }
